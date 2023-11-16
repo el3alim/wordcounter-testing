72ddb0010b380480cd31599806579907787d0ca9
unknown <el3alim@gmail.com>
2023-11-16 13:05:00 +0200

    reactjs testing a word and character counter project

---
+++ src/components/__test__/Counter.test.js
@@ -0,0 +1,51 @@
+import { render, screen, fireEvent } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import "@testing-library/jest-dom";
+import { Counter } from "../Counter";
+
+describe("Counter Component Test", () => {
+  test("render the textarea", () => {
+    const { getByPlaceholderText } = render(<Counter />);
+    const textArea = getByPlaceholderText("Type or paste your text");
+    expect(textArea).toBeTruthy();
+  });
+
+  test("render the character length", () => {
+    const { getByTestId } = render(<Counter />);
+    const charLength = getByTestId("charLength");
+    expect(charLength.innerHTML).toBe("Character: 0");
+  });
+
+  test("render the word result", () => {
+    render(<Counter />);
+    const wordLength = screen.getByText("Word: 0");
+    expect(wordLength).toBeInTheDocument();
+  });
+
+  test("change text area and update result", () => {
+    render(<Counter />);
+    const textArea = screen.getByTestId("textArea");
+    const charLength = screen.getByTestId("charLength");
+    const wordLength = screen.getByTestId("wordLength");
+
+    userEvent.type(textArea, "Mustafa");
+    expect(charLength.innerHTML).toBe("Character: 7");
+    expect(wordLength.innerHTML).toBe("Word: 1");
+  });
+
+  test("clear textarea and update result", () => {
+    render(<Counter />);
+    const textArea = screen.getByTestId("textArea");
+    const clearBtn = screen.getAllByTestId("clearBtn");
+    const charLength = screen.getByTestId("charLength");
+    const wordLength = screen.getByTestId("wordLength");
+
+    userEvent.type(textArea, "Mustafa");
+    expect(charLength.innerHTML).toBe("Character: 7");
+    expect(wordLength.innerHTML).toBe("Word: 1");
+
+    fireEvent.click(clearBtn[0]);
+    expect(charLength.innerHTML).toBe("Character: 0");
+    expect(wordLength.innerHTML).toBe("Word: 0");
+  });
+});
---
+++ src/components/__test__/Joke.test.js
@@ -0,0 +1,10 @@
+import { render, waitFor } from "@testing-library/react";
+import { Joke } from "../Joke";
+
+describe("Test Joke Component", () => {
+  test("render joke text", async () => {
+    const { getByTestId } = render(<Joke />);
+    const jokeHeadline = getByTestId("jokeHeadline");
+    await waitFor(() => expect(jokeHeadline.innerHTML).toBeTruthy());
+  });
+});
